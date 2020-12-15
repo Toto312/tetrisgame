23ce09dd8addda45c42d02ed516915a8504a5029
Toto312 <66850618+Toto312@users.noreply.github.com>
2020-12-15 18:37:13 -0300

    updating main.cpp

--- main.cpp
+++ main.cpp
@@ -12,47 +12,148 @@
 //Point options
 enum Color { RED, GREEN, BLUE };
 
+struct Position2D {
+	int x, y;
+};
+
 //Point struct
 struct Point {
-	int x, y;
+	Position2D pos;
 	Color color;
+	Position2D lastMove;
 } player;
 
-//Other points. The first variable is the value of x, and the second is y. The third variable is x, and the fourth variable is y, etc.
-int others[] = { 0, 0, 0, 0, 0, 0 };
+Point others1[3];
 
 //variables for other poinbts
-int otherY = MOVE(0); // cambiar el 3er numero para moverlo
-int otherX = MOVE(0);
+int otherY;
+int otherX;
 
 //world options
-float gravity = 0;
+float gravityScale = 0;
+
+/*This function is used to override the "gravity" of the game. 
+This is so that, when gravity lowers a block to the point, it does not lower more if the player presses the down button at that precise moment.*/
+bool playerShouldUseBottonDown;
+
 
 void init()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 
+	//---------------------------------------------------------------
+	//|                      Funciones del juego			|
+	//---------------------------------------------------------------
+
+
+
+	//init map
+	/*int map[50][20] = {
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};*/
+
+
+	int playerInit_x = 10;
+	int playerInit_y = 20;
+	player.lastMove.x = playerInit_x;
+	player.lastMove.y = playerInit_y;
+
+
+
+	int othersInit_x = 10;
+	int othersInit_y = 0;
+	others1[0].lastMove.x = othersInit_x;
+	others1[0].lastMove.y = othersInit_y;
+
 	//playerInit
-	player.x = MOVE(10);
-	player.y = MOVE(20);
+	player.pos.x = MOVE(playerInit_x);
+	player.pos.y = MOVE(playerInit_y);
 	player.color = RED;
 
 	//Others init
-	others[0] = MOVE(10);
-	others[1] = MOVE(0);
+	others1[0].pos.x = MOVE(othersInit_x);
+	others1[0].pos.y = MOVE(othersInit_y);
+	others1[0].color = GREEN;
+
+	/*map[playerInit_y][playerInit_x] = 1;
+	map[othersInit_y][othersInit_x] = 1;*/
+
+	playerShouldUseBottonDown = true;
+
 }
 
-//OpenGL options
-void resize(int w, int h)
-{
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, w, 0, h);
 
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+
+void gravityWrld()
+{
+	if (player.pos.y > LOW_COORDENADE)
+	{
+		gravityScale += 0.001;
+	}
+	else {
+		gravityScale = 0;
+	}
+
+	if (gravityScale >= 1 && player.pos.y > LOW_COORDENADE && playerShouldUseBottonDown)
+	{
+		if (!(others1[0].pos.x == player.pos.x && others1[0].pos.y == player.pos.y - 24))
+		{
+			playerShouldUseBottonDown = false;
+			player.pos.y -= 24;
+			gravityScale = 0;
+		}
+	}
+
+	playerShouldUseBottonDown = true;
 }
 
 //Render function
@@ -66,15 +167,19 @@ void display()
 
 	glPushMatrix();
 	glBegin(GL_POINTS);
+
+	//set the color
 	if (player.color == RED)
 	{
 		glColor3f(1.0, 0.0, 0.0);
 	}
-	glVertex2i(player.x, player.y);
 
+	//set the position of the player point
+	glVertex2i(player.pos.x, player.pos.y);
+
+	//set color and position of other block
 	glColor3f(0.0, 1.0, 0.0);
-	glVertex2i(others[0], others[1]);
-
+	glVertex2i(others1[0].pos.x, others1[0].pos.y);
 	glEnd();
 	glPopMatrix();
 
@@ -82,28 +187,13 @@ void display()
 
 
 	//-----------------------------------------------
-	//             Gravity Options					|	
+	//             Gravity Options			|	
 	//-----------------------------------------------
 
 
-	if (player.y > LOW_COORDENADE)
-	{
-		gravity += 0.001;
-	}
-	else {
-		gravity = 0;
-	}
+	gravityWrld();
 
-	if (gravity >= 1 && player.y > LOW_COORDENADE)
-	{
-		if (!(others[0] == player.x && others[1] == player.y - 24))
-		{
-			player.y -= 24;
-			gravity = 0;
-		}
-	}
-
-	std::cout << "Value of gravity: " << gravity << std::endl;
+	//map[][]
 }
 
 //Keys for input the keyboard
@@ -111,50 +201,64 @@ void specialKeys(int key, int x, int y)
 {
 	switch (key) {
 	case GLUT_KEY_DOWN:
-		if (player.y != LOW_COORDENADE)
+		if (player.pos.y != LOW_COORDENADE && playerShouldUseBottonDown)
 		{
-			if (!(others[0] == player.x - 24 && others[1] == player.y - 24))
+			if (!(others1[0].pos.x == player.pos.x - 24 && others1[0].pos.y == player.pos.y - 24))
 			{
-				player.y -= 24;
+				player.pos.y -= 24;
 			}
 		}
 		break;
 	case GLUT_KEY_RIGHT:
-		if (player.y != LOW_COORDENADE) {
-			if (!(others[0] == player.x - 24 && others[1] == player.y - 24))
+		if (player.pos.y != LOW_COORDENADE) {
+			if (!(others1[0].pos.x == player.pos.x - 24 && others1[0].pos.y == player.pos.y - 24))
 			{
-				player.x += 24;
+				player.pos.x += 24;
 			}
 		}
 		break;
 	case GLUT_KEY_LEFT:
-		if (player.y != LOW_COORDENADE) {
-			if (!(others[0] == player.x - 24 && others[1] == player.y - 24))
+		if (player.pos.y != LOW_COORDENADE) {
+			if (!(others1[0].pos.x == player.pos.x - 24 && others1[0].pos.y == player.pos.y - 24))
 			{
-				player.x -= 24;
+				player.pos.x -= 24;
 			}
 		}
 		break;
 	}
+
 }
 
-void Timer(int value)
-{
-	glutPostRedisplay();
-	glutTimerFunc(1, Timer, 0);
-}
-
+//The OpenGL keyboard
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key) {
 	//Is the value ascii of "r". This means that if the player input the letter "r", the point is going to reset its position
 	case 114:
-		player.y = MOVE(20);
-		player.x = MOVE(10);
+		player.pos.y = MOVE(20);
+		player.pos.x = MOVE(10);
 		break;
 	}
 }
 
+//the OpenGL resize
+void resize(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, w, 0, h);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+//the OpenGL timer
+void Timer(int value)
+{
+	glutPostRedisplay();
+	glutTimerFunc(1, Timer, 0);
+}
 
 //The main function of the program
 int main(int argc, char* argv[])
